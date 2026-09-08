b5e2dd7355407549d9c645488e03f41aa633e5bc
aloshi2003 <alosiousalbin90@gmail.com>
2026-09-08 15:52:51 +0530

    Updated notifications and styles in employee side

--- src/App.jsx
+++ src/App.jsx
@@ -142,6 +142,7 @@ const EmployeeProfile = lazy(() => import("./employee/pages/Profile"));
 const EmployeeWFH = lazy(() => import("./employee/pages/WFH"));
 const EmployeeTaskReports = lazy(() => import("./employee/pages/TaskReports"));
 const EmployeeTasks = lazy(() => import("./employee/pages/Tasks"));
+const EmployeeNotifications = lazy(() => import("./employee/pages/Notifications"));
 const AttendanceRequests = lazy(
   () => import("./employee/pages/AttendanceRequests"),
 );
@@ -377,6 +378,7 @@ function App() {
           <Route path="my-tasks" element={<EmployeeTaskReports />} />
           <Route path="task-reports" element={<TaskReports />} />
           <Route path="tasks" element={<EmployeeTasks />} />
+          <Route path="notifications" element={<EmployeeNotifications />} />
           <Route path="profile" element={<EmployeeProfile />} />
           <Route path="attendance-requests" element={<AttendanceRequests />} />
           <Route path="organizations" element={<Organizations />} />
